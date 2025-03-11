4ea1c79f741aadc1179311322b5bb3aa316642b2
David1712pc <david1237891712@gmail.com>
2025-03-10 19:45:25 -0500

    Eliminando archivos repetidos

--- AppWeb/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('resultados/', views.mostrar_resultados, name='resultados'),
-    path('saldo_inicial/', views.elegir_saldos, name='saldo_inicial'),
-]
