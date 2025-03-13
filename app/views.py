fe1bdeb9c7b624516985547d2583562b8868ae53
David Caruzo Cieza <david1237891712@gmail.com>
2025-03-12 22:47:55 -0500

    QualityPassed

--- app/views.py
+++ app/views.py
@@ -590,10 +590,6 @@ def elegir_saldos(request):
         'saldo_form': saldo_form,
         'saldos': resultado,
     }
-    def infinite_recursion():
-        return infinite_recursion()
-
-    infinite_recursion()
-    
+  
     return render(request, 'saldo_inicial.html', context)
     
