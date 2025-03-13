f2b8397d6d177b6ac9a4a3d718219b8727bad98b
David Caruzo Cieza <david1237891712@gmail.com>
2025-03-12 22:37:32 -0500

    Update views.py
    Probar QualityFailed

--- app/views.py
+++ app/views.py
@@ -593,6 +593,7 @@ def elegir_saldos(request):
     def infinite_recursion():
         return infinite_recursion()
 
-
+    infinite_recursion()
+    
     return render(request, 'saldo_inicial.html', context)
     
